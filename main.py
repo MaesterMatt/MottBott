5b287b61c7a766a81a5176f4dc98a4560a88495a
Matthew Ng <matthewjames.ng@gmail.com>
2021-07-04 01:32:54 +0000

    adding hau sux/doesnt sux and if sujung says darrren it's 100% a heart

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import discord
 import os
 import time
+import random
 
 client = discord.Client()
 
@@ -17,9 +18,14 @@ async def on_message(message):
         return
 
     #darren nauseous react
-    if 'darren' in message.content.lower() or message.author == "misterjuicy#8092":
-        emoji = '\U0001F922'#'\N{NAUSEATED FACE}'
-        await message.add_reaction(emoji)
+    if 'darren' in message.content.lower():
+        emoji = '\U0001F922'#'\N{2764}'
+        emoji2 = '\U00002764'
+        darren = random.randint(0, 10)
+        if str(message.author) == "$hwayjung#9949":
+          await message.add_reaction(emoji2)
+        else:
+          await message.add_reaction(emoji2 if darren == 0 else emoji)
 
     #For Julia Nuts emoji reaction
     global juwi_last
@@ -33,6 +39,13 @@ async def on_message(message):
         emoji = '\U0001F1F8'
         await message.add_reaction(emoji)
         juwi_last = time.time()
+
+    #Hau sux
+    if 'hau' in message.content.lower():
+      #msg = str("hau sux")
+      hau = bool(random.getrandbits(1))
+      
+      await message.channel.send('sux' if hau else 'doesn\'t sux')
 
 
 client.run(os.getenv('TOKEN'))
