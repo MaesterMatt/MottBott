9d4bb980ce1ca0c464d63e61be69a4c13fad5915
Matthew Ng <matthewjames.ng@gmail.com>
2021-07-05 08:05:35 +0000

    enabled gif on member join as well as new messages

--- main.py
+++ main.py
@@ -10,17 +10,15 @@ import logging
 
 intents = discord.Intents.default()
 intents.members = True
-
 client = discord.Client(intents=intents)
-#welcome_channel = discord.Guild.system_channel.TextChannel
-#print(welcome_channel)
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 juwi_last = time.time()-60
 @client.event
-async def on_message(message):
+async def on_message(message):      
     if message.author == client.user:
         return
 
@@ -57,18 +55,22 @@ async def on_message(message):
 #Public Welcome
 @client.event
 async def on_member_join(member):
-    print("Member Detected") #495284966876512258
-    newUserDMMessage = "Welcome to Spirit, " + member.name + "!\nPlease check out the roles in #self-add-roles"
+    marc_welcome = "https://media.discordapp.net/attachments/495284966876512258/829984477727424512/ezgif.com-video-to-gif-2.gif"
+    mylist = ["**Welcome to Spirit!** We're excited to see you ", "**Welcome to Spirit** BINCH... Enjoy your stay ", "**SSSUUUUUUUHHHHHHHHHHHHH** "]
+    rand_quote = mylist[random.randint(0, len(mylist)-1)]
+    newUserDMMessage = rand_quote + member.name + "!\n"
+    self_add_roles = client.get_channel(717216767222480896)
+    print(self_add_roles.name)
+    newUserDMMessage2 = 'Please check out the roles in {0.mention}'.format(self_add_roles)
+    
     print("Recognized that " + member.name + " joined")
-    #member.add_roles("spies", )
-    # role = discord.utils.get(member.guild.roles, name="spies")
-    # await member.add_roles(role)
+  
     role = discord.utils.get(member.guild.roles, id=727473839268691968)
     await member.add_roles(role)
     time.sleep(1)
     await member.guild.system_channel.send(newUserDMMessage)
-    #await client.send_message(discord.Object(id='CHANNELID'), 'Welcome!')
-    # print("Sent message to " + member.name)
-    # print("Sent message about " + member.name + " to #CHANNEL")
+    await member.guild.system_channel.send(file=discord.File('marc.gif'))
+    await member.guild.system_channel.send(newUserDMMessage2)
+
 
 client.run(os.getenv('TOKEN'))
