3db61a4dc799137a490c936769e8bedf2dcf8d19
Matthew Ng <matthewjames.ng@gmail.com>
2021-07-08 16:22:54 +0000

    more JC messages

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def on_message(message):
       hau = bool(random.getrandbits(1))
       await message.channel.send('sux' if hau else 'doesn\'t sux')
 
-    mylist = ["jc...........", "JCJCJCJCJCJCJCJC", ".................JC!", "down >:)", "Oh sorry I'm busy", "Run without me :(", "I'm out right now", "How about in like an hour?", "Im going to dentist ...", "I WNA NAP", "I’m at hospital"]
+    mylist = ["JCJCJCJCJCJCJCJC", ".................JC!", "down >:)", "Oh sorry I'm busy", "Run without me :(", "I'm out right now", "How about in like an hour?", "Im going to dentist ...", "I WNA NAP", "I’m at hospital", "IM GETTING THE BEE OUT STILL", "It's too late for JC now, imma go to zak"]
     if 'jc' in message.content.lower():
       rand_quote = mylist[random.randint(0, len(mylist)-1)]
       await message.channel.send(rand_quote)
