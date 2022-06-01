89edbbb6dd15c02af28850ffc6a1f3ecc5160dfc
Matthew Ng <matthewjames.ng@gmail.com>
2022-06-01 23:33:35 +0000

    debug

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ savewho = []
 @client.event
 async def on_message(message):  
     msg_lower = message.content.lower()
-    msg_noyt = "".join(msg_lower[0].replace("?","").replace(".","")).split()
+    msg_noyt = "".join(msg_lower.replace("?","").replace(".","")).split()
+    print(msg_noyt)
+    msg_noyt = msg_noyt[0]
 
     if message.author == client.user:
         return
